3f39d7123f9915feaffbb6b64c3f5013c78961bf
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-07-03 12:50:20 +0600

    feat: add mongodb connection function using mongoose

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+const connectMongoDB = async () => {
+  try {
+    if (mongoose.connection.readyState >= 1) return;
+
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectMongoDB;
